09121a448a68291995818ab02ea2a46223d331eb
Romeo <romeo@framework.tools>
2022-01-06 19:12:42 +1030

    started job components

---
+++ frontend/src/components/jobs/JobArea.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+import Job from "@components/jobs/Job.svelte"
+
+export let heading: string
+export let jobs: Job[]
+
+</script>
+
+<div class="main">
+    <div class="heading">
+        {heading}
+    </div>
+    <div class="jobs-wrapper">
+        {#each jobs as job}
+            <Job 
+                job={
+                    {
+                        title: job.title,
+                        icon: job.icon,
+                        attributes: {
+                            icon: any,
+                            text: string,
+                            color: string,
+                        }
+                    }
+                }
+            />
+        {/each}
+    </div>
+</div>
+
+<style lang="stylus">
+
+
+</style>
